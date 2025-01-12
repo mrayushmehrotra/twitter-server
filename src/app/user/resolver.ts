2377e1c63cb3acd4a09253acfe71674726829f79
ayushM <ayushmehrotra099@gmail.com>
2025-01-12 22:58:14 +0530

    added jwt

--- src/app/user/resolver.ts
+++ src/app/user/resolver.ts
@@ -1,7 +1,76 @@
+import axios from "axios";
+import { prismaClient } from "../../clients/db";
+import JWTService from "../../services/jwt";
+import { PrismaClient } from "@prisma/client";
+
+interface GoogleTokenResult {
+  iss?: string;
+  nbf?: string;
+  aud?: string;
+  sub?: string;
+  email: string;
+  email_verified: string;
+  azp?: string;
+  name?: string;
+  picture?: string;
+  given_name: string;
+  family_name?: string;
+  iat?: string;
+  exp?: string;
+  jti?: string;
+  alg?: string;
+  kid?: string;
+  typ?: string;
+}
+
 export const resolvers = {
   Query: {
     verifyGoogleToken: async (parent: any, { token }: { token: string }) => {
-      return token;
+      if (!token) {
+        throw new Error("Token is required.");
+      }
+      try {
+        const googleOauthURL = new URL(
+          "https://oauth2.googleapis.com/tokeninfo",
+        );
+        googleOauthURL.searchParams.set("id_token", token);
+
+        const { data } = await axios.get<GoogleTokenResult>(
+          googleOauthURL.toString(),
+          {
+            responseType: "json",
+          },
+        );
+        const user = await PrismaClient.user.findUnique({
+          where: { email: data.email },
+        });
+
+        if (!user) {
+          await prismaClient.user.create({
+            data: {
+              email: data.email,
+              firstName: data.given_name,
+              lastName: data.family_name,
+              profileImageURL: data.picture,
+            },
+          });
+        }
+        const userInDb = await PrismaClient.user.findUnique({
+          where: {
+            email: data.email,
+          },
+        });
+        if (!userInDb) {
+          throw new Error("User with email not found");
+        }
+        const token = await JWTService.generateTokenForUser(user?.id);
+
+        console.log(data);
+        return "ok";
+      } catch (error) {
+        console.error("Error verifying Google token:", error);
+        throw new Error("Failed to verify Google token.");
+      }
     },
   },
 };
