8a32944d913c0b4387491c7894749df087d39997
ayushM <ayushmehrotra099@gmail.com>
2025-02-03 17:03:35 +0530

    added redis rate limit

--- src/services/tweet.ts
+++ src/services/tweet.ts
@@ -1,4 +1,5 @@
 import { prismaClient } from "../clients/db";
+import { redisClient } from "../clients/redis/index";
 
 export interface CreateTweetPayload {
   content: string;
@@ -7,14 +8,22 @@ export interface CreateTweetPayload {
 }
 
 class TweetService {
-  public static createTweet(data: CreateTweetPayload) {
-    return prismaClient.tweet.create({
+  public static async createTweet(data: CreateTweetPayload) {
+    const rateLimitFlag = await redisClient.get(
+      `RATE_LIMIT:TWEET:${data.userId}`,
+    );
+    if (rateLimitFlag) throw new Error("please wait...");
+    const tweet = prismaClient.tweet.create({
       data: {
         content: data.content,
         imageURL: data.imageURL,
         author: { connect: { id: data.userId } },
       },
     });
+    await redisClient.setex(`RATE_LIMIT:TWEET:${data.userId}`, 10, 1);
+
+    await redisClient.del("ALL_TWEETS");
+    return tweet;
   }
   public static getAllTweets() {
     return prismaClient.tweet.findMany({ orderBy: { createdAt: "desc" } });
